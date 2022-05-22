8780fa76ad95f4c5be59b503a608547d0bf0fa1c
Laura Walters <laura.walters1098@yahoo.com>
2022-05-22 13:31:59 +0100

    routes

--- src/components/OffcanvasData.jsx
+++ src/components/OffcanvasData.jsx
@@ -9,19 +9,19 @@ import { FaPhoneSquare } from 'react-icons/fa';
 export const OffcanvasData = [
     {
         title: 'Home',
-        path: '/',
+        path: '/laura-walters-portfolio/',
         icon: <HomeIcon />,
         cName: 'nav-text'
     },
     {
         title: 'Repositories',
-        path: '/repositories',
+        path: '/laura-walters-portfolio/repositories',
         icon: <FaGitAlt />,
         cName: 'nav-text'
     },
     {
         title: 'Contact',
-        path: '/contact',
+        path: '/laura-walters-portfolio/contact',
         icon: <FaPhoneSquare />,
         cName: 'nav-text'
     }
